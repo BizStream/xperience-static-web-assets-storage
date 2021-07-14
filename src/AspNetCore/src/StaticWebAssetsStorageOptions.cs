d6ff622e609ced6ea7268e4e9cb53a5125b8f485
Samuel Steele <ssteele@bizstream.com>
2021-07-13 23:55:00 -0400

    Initial commit

---
+++ src/AspNetCore/src/StaticWebAssetsStorageOptions.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using BizStream.Kentico.Xperience.AspNetCore.StaticWebAssetsStorage.IO;
+using Microsoft.Extensions.Hosting;
+
+namespace BizStream.Kentico.Xperience.AspNetCore.StaticWebAssetsStorage
+{
+
+    /// <summary> Represents options that configure the behavior of <see cref="StaticWebAssetsStorageProvider"/>s. </summary>
+    public class StaticWebAssetsStorageOptions
+    {
+
+        /// <summary> The names of <see cref="Environments"/> in which to register <see cref="StaticWebAssetsStorageProvider"/>s. </summary>
+        /// <value> <see cref="Environments.Development"/>. </value>
+        public IList<string> EnvironmentNames { get; } = new List<string> { Environments.Development };
+
+    }
+
+}
---
+++ src/AspNetCore/src/StaticWebAssetsStorageStrings.cs
@@ -0,0 +1,13 @@
+namespace BizStream.Kentico.Xperience.AspNetCore.StaticWebAssetsStorage
+{
+
+    /// <summary> Constant strings. </summary>
+    public static class StaticWebAssetsStorageStrings
+    {
+
+        /// <summary> The <c>BasePath</c> of the Kentico RCL assembly. </summary>
+        public const string KenticoMvcRCLBasePath = "_content/Kentico.Content.Web.Rcl";
+
+    }
+
+}
